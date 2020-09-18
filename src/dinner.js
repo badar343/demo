69cdfa22109ba1dfc500488323430899b34464f7
badar343 <66517064+badar343@users.noreply.github.com>
2020-09-18 15:56:35 +0500

    uploaded demo app

---
+++ src/dinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Dinner(props){
+    return(
+        
+        <h1>Today we are Serving {props.dishName} & {props.sweetDish} </h1>
+    )
+}
+export default Dinner
